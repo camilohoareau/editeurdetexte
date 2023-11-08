17560fc692113fd14a1ce2de5ccd8af6a7a43c60
Camilo Hoareau-Deram <camilo.hoareau-deram@etu.umontpellier.fr>
2023-11-08 16:00:17 +0100

    ajout remplacer
    Update 6 files
    
    - /.idea/.gitignore
    - /.idea/compiler.xml
    - /.idea/encodings.xml
    - /.idea/jarRepositories.xml
    - /.idea/misc.xml
    - /.idea/vcs.xml
    
    ajout remplacer avec commentaire
    
    ajout remplacer

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,31 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        //ajouter exception
+        if(parameters.length < 3) {
+            System.err.println("Format attendu : remplacer;depart;fin;chaine");
+            return;
+        }
+        //creation du texte
+        String texte;
+        if(parameters.length < 4) {
+            texte = "";
+        } else {
+            texte = parameters[3];
+        }
+        //remplacement du texte
+        this.document.remplacer(Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2]),texte);
+        super.executer();
+    }
+
+}
